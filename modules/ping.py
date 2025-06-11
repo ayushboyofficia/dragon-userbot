c3028b45b2189e524473c2c7d7440632eb66c502
ayushboyofficia <160393536+ayushboyofficia@users.noreply.github.com>
2025-06-11 22:40:35 +0530

    Update ping.py

--- modules/ping.py
+++ modules/ping.py
@@ -1,1 +1,11 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+import time
 
+@Client.on_message(filters.command("ping", prefixes=".") & filters.me)
+async def pingme(client: Client, message: Message):
+    start = time.time()
+    m = await message.reply("🏓 Pinging...")
+    end = time.time()
+    ping_time = round((end - start) * 1000)
+    await m.edit(f"🏓 Pong! `{ping_time} ms`")
